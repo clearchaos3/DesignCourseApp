3029c2058e6d2e3df6fa2cacf195fd00d7c0a90b
clearchaos3 <clearchaos3@gmail.com>
2020-07-30 09:52:50 -0500

    Initial Commit

---
+++ Shared/DesignCourseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCourseAppApp.swift
+//  Shared
+//
+//  Created by Ryan Lee on 7/30/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCourseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
